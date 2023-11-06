15ddf38a119ed86da6168b8dbde9e3b3a53b3e94
precillamonthero <precilla.m@ambertag.com>
2023-11-06 09:49:23 +0530

    final commit

---
+++ Mytestngproject/src/com/testngproject/Whatsapplogin.java
@@ -0,0 +1,18 @@
+package com.testngproject;
+
+import org.testng.annotations.Test;
+
+public class Whatsapplogin {
+	
+	@Test
+	public void loginwithvalidcrendentials()
+	{
+		System.out.println("Hi");
+		System.out.println("Hello");
+		System.out.println("How are you?");
+		System.out.println("What is your name?");
+		System.out.println("My name is Aditya");
+		
+	}
+
+}
